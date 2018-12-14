e409f3735615f61d8f5e815e609ce0a0b1fbd9fa
chenxingyu0830 <chenxingyu0830@gmail.com>
2018-12-14 13:42:38 +0800

    第二次提交

--- main.js
+++ main.js
@@ -1,23 +1,46 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-let n = 1
+let n
+imagesInitial()
 setInterval(()=>{
-  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
-    .one('transitionend', (e)=>{
-      $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+  makeLeave(getImage(n))
+    .one('transitionend',(e)=>{
+        makeEnter($(e.currentTarget))
+      })
+  makeCurrent(getImage(n+1))
   n += 1
 },3000)
 
 
+
+
+
+
+
+
+
+//封装的函数
+function getImage(n){
+  return $(`.images > img:nth-child(${x(n)})`)
+}
 function x(n){
   if(n>3){
     n = n%3
-    if (n===0){
-      n =3
+    if(n === 0){
+      n = 3
     }
-  } // n = 1 2 3
+  } //n = 1 2 3
   return n
+}
+function imagesInitial(){
+  n = 1
+  $(`.images > img:nth-child(${n})`).addClass('current')
+  .siblings().addClass('enter')
+}
+function makeCurrent($node){
+  return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+  return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+  return $node.removeClass('leave current').addClass('enter')
 }
