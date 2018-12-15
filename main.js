421bbfd7c2d5ad0f9cc65f810edf89af6e75ffbc
chenxingyu0830 <chenxingyu0830@gmail.com>
2018-12-15 21:12:48 +0800

    第三个版本

--- main.js
+++ main.js
@@ -1,62 +1,71 @@
-let n
-imagesInitial()
-let timer = setInterval(()=>{
-  makeLeave(getImage(n))
-    .one('transitionend',(e)=>{
-        makeEnter($(e.currentTarget))
-      })
-  makeCurrent(getImage(n+1))
-  n += 1
-},3000)
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
 
-document.addEventListener('visibilitychange',function(e){ //添加计时器防止切页面导致BUG
-  console.log(document.hidden)
-  if(document.hidden){
-    window.clearInterval(timer)
-  }else{
-    timer = setInterval(()=>{
-      makeLeave(getImage(n))
-        .one('transitionend',(e)=>{
-            makeEnter($(e.currentTarget))
-          })
-      makeCurrent(getImage(n+1))
-      n += 1
-    },3000)
-  }
+makeFakeSlides()
+$slides.css({transform:'translateX(-400px)'})
+bindEvents()
+$(next).on('click',function(){
+  goToSlide(current + 1)
+})
+$(previous).on('click',function(){
+  goToSlide(current - 1)
+})
+let timer = setInterval(function(){
+  goToSlide(current + 1)
+},2000)
+$('.container').on('mouseenter',function(){
+  window.clearInterval(timer)
+})
+$('.container').on('mouseleave',function(){
+  timer = setInterval(function(){
+    goToSlide(current + 1)
+  },2000)
 })
 
 
-
-
-
-
-
-
-
-//封装的函数
-function getImage(n){
-  return $(`.images > img:nth-child(${x(n)})`)
+//函数封装
+function makeFakeSlides(){
+  let $firstCopy = $images.eq(0).clone(true)
+  let $lastCopy = $images.eq($images.length-1).clone(true)
+  $slides.append($firstCopy)
+  $slides.prepend($lastCopy)
 }
-function x(n){
-  if(n>3){
-    n = n%3
-    if(n === 0){
-      n = 3
-    }
-  } //n = 1 2 3
-  return n
+function bindEvents(){
+  $('#buttonWrapper').on('click','button',function(e){
+    let $button = $(e.currentTarget)
+    let index = $button.index()
+    goToSlide(index)
+  })
 }
-function imagesInitial(){
-  n = 1
-  $(`.images > img:nth-child(${n})`).addClass('current')
-  .siblings().addClass('enter')
-}
-function makeCurrent($node){
-  return $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node){
-  return $node.removeClass('enter current').addClass('leave')
-}
-function makeEnter($node){
-  return $node.removeClass('leave current').addClass('enter')
+function goToSlide(index){  //重要
+  if(index >= $buttons.length){
+    index = 0
+  }else if(index < 0){
+    index = $buttons.length -1
+  }
+  if(current === $buttons.length - 1 && index === 0){
+    //说明你是最后一张到第一张
+    console.log(1)
+    $slides.css({transform: `translateX(${-($buttons.length + 1) * 400}px)`})
+      .one('transitionend',function(){
+        $slides.hide().offset()
+        $slides.css({transform:`translateX(${-(index+1)*400}px)`}).show()
+      })
+  }else if(current === 0 && index === $buttons.length - 1){
+    //说明你是第一张直接到最后一张
+    console.log(2)
+    $slides.css({transform: 'translateX(0px)'})
+      .one('transitionend',function(){
+        $slides.hide().offset()
+        // .offset() 可以触发 re-layout，这是一个高级技术，删掉这行你就会发现 bug，所以只能加上这一行。
+        $slides.css({transform:`translateX(${-(index+1)*400}px)`}).show()
+      })
+  }else{
+    //正常顺序
+    console.log(3)
+    $slides.css({transform:`translateX(${-(index+1)*400}px)`})
+  }
+  current = index
 }
